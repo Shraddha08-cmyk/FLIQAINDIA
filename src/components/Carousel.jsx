05be8c4115e1bb91d9d234f6462b23936df404d3
Shraddha08-cmyk <shraddhatiwari8175@gmail.com>
2023-08-03 10:49:44 +0530

    new

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-} from 'mdb-react-ui-kit';
+// import React from 'react';
+// import {
+//   MDBCarousel,
+//   MDBCarouselItem,
+// } from 'mdb-react-ui-kit';
 
-export default function Carousel() {
-  return (
-    <MDBCarousel showControls showIndicators>
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={1}
-        src='https://mdbootstrap.com/img/new/slides/041.jpg'
-        alt='...'
-      />
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={2}
-        src='https://mdbootstrap.com/img/new/slides/042.jpg'
-        alt='...'
-      />
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={3}
-        src='https://mdbootstrap.com/img/new/slides/043.jpg'
-        alt='...'
-      />
-    </MDBCarousel>
-  );
-}
+// export default function Carousel() {
+//   return (
+//     <MDBCarousel showControls showIndicators>
+//       <MDBCarouselItem
+//         className='w-100 d-block'
+//         itemId={1}
+//         src='https://mdbootstrap.com/img/new/slides/041.jpg'
+//         alt='...'
+//       />
+//       <MDBCarouselItem
+//         className='w-100 d-block'
+//         itemId={2}
+//         src='https://mdbootstrap.com/img/new/slides/042.jpg'
+//         alt='...'
+//       />
+//       <MDBCarouselItem
+//         className='w-100 d-block'
+//         itemId={3}
+//         src='https://mdbootstrap.com/img/new/slides/043.jpg'
+//         alt='...'
+//       />
+//     </MDBCarousel>
+//   );
+// }
